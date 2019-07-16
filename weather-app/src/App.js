9a3eddf92ae861385debc92697cb8035c71edbc5
krisstets <krist.stecz2014@gmail.com>
2019-07-16 14:09:51 +0300

    feat: add components

---
+++ weather-app/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import Title from "./components/Title";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <Title />
+        <Form />
+        <Weather />
+      </div>
+    );
+  };
+}
+
+export default App;
---
+++ weather-app/src/components/Weather.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class Weather extends Component {
+    render() {
+        return(
+            <div>weather</div>
+        );
+    }
+}
