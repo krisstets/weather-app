a46bb21323504a2302f9b28dcde4320e216f227c
krisstets <krist.stecz2014@gmail.com>
2019-07-17 13:15:32 +0300

    feat: add weather data

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -8,7 +8,15 @@ const API_KEY = "197bb3bc90d1a83fe282b5e8b3d6bb29";
 
 
 class App extends Component {
+  state ={ 
+    temperature: undefined,
+    city: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
   getWeather = async (e) => {
+  
     e.preventDefault();
 
     const city = e.target.elements.city.value;
@@ -17,14 +25,39 @@ class App extends Component {
     const api_call = await fetch(URL);
     const data = await api_call.json();
 
-    console.log(data);
+    if (city && country) {
+      this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the value"
+      });
+    }
   }
   render() {
     return(
       <div>
         <Title />
         <Form getWeather = {this.getWeather}/>
-        <Weather />
+        <Weather
+         temperature={this.state.temperature} 
+         city={this.state.city}
+         country={this.state.country}
+         humidity={this.state.humidity}
+         description={this.state.description}
+         error={this.state.error}
+         />
       </div>
     );
   };
