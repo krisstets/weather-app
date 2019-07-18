7550cf18a974fdfafaaffe6dc91d025a4d39a090
krisstets <krist.stecz2014@gmail.com>
2019-07-18 16:09:23 +0300

    feat: tranceform lat and long into location

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -4,7 +4,9 @@ import Title from "./components/Title";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
+
 const API_KEY = "197bb3bc90d1a83fe282b5e8b3d6bb29";
+const GEOLOCATION_KEY = "AIzaSyDL12P-3ezGlsQXbZc4Jdap43Nbw50cUTg ";
 
 
 class App extends Component {
@@ -21,9 +23,32 @@ class App extends Component {
 
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const URL = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+
+    const URL = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`;
     const api_call = await fetch(URL);
     const data = await api_call.json();
+
+    function showPosition(position) {
+      const long = position.coords.longitude;
+      const lat = position.coords.latitude;
+      urlRequest(long, lat);
+      console.log(position);
+    } 
+
+   async function urlRequest(long, lat) {
+    const Location_URL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${GEOLOCATION_KEY}`;
+    const apiCall = await fetch(Location_URL);
+    const data = await apiCall.json();
+    console.log(data);
+   }
+
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      }
+    
+
+   
+  
 
     if (city && country) {
       this.setState({
--- weather-app/src/components/Weather.js
+++ weather-app/src/components/Weather.js
@@ -5,7 +5,7 @@ export default class Weather extends Component {
         return(
             <div>
               { this.props.city && this.props.country && <p> Location: { this.props.city }, { this.props.country }</p> }
-              { this.props.temperature && <p>Temperature: { this.props.temperature }</p> } 
+              { this.props.temperature && <p>Temperature: { this.props.temperature } °C</p> } 
               { this.props.humidity && <p> Humidity: { this.props.humidity }</p> }
               { this.props.description && <p> Condition:   { this.props.description }</p> }
               { this.props.error && <p>this.props.error</p> } 
