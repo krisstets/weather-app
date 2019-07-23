a868882834853d94294a2f1d8b6f5ae64c4e740f
krisstets <krist.stecz2014@gmail.com>
2019-07-23 15:16:53 +0300

    feat: add method for local storage

--- weather-app/src/components/Weather.js
+++ weather-app/src/components/Weather.js
@@ -5,8 +5,8 @@ export default class Weather extends Component {
         return(
             <div>
               { this.props.city && this.props.country && <p> Location: { this.props.city }, { this.props.country }</p> }
-              { this.props.temperature && <p>Temperature: { this.props.temperature } °C</p> } 
-              { this.props.humidity && <p> Humidity: { this.props.humidity }</p> }
+              { this.props.temperature && <p>Temperature: { this.props.temperature }°C</p> } 
+              { this.props.humidity && <p> Humidity: { this.props.humidity }%</p> }
               { this.props.description && <p> Condition:   { this.props.description }</p> }
               { this.props.error && <p>this.props.error</p> } 
             </div>
